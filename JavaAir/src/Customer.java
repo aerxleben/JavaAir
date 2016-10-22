c860f024dc9d5092ad3d8a3c50abc0b2e8c9a1a9
Steve Jia <ssjia@pnw.edu>
2016-10-22 12:13:38 -0500

    added validate and set attribute value methods

--- JavaAir/src/Customer.java
+++ JavaAir/src/Customer.java
@@ -18,7 +18,7 @@ import javax.swing.JOptionPane;
 public class Customer{
  
    private ArrayList<String> customerInfoList;
-   private String firstName;		//index 0
+   /*private String firstName;		//index 0
    private String lastName;             //index 1
    private String dateOfBirth;		//index 2
    private String gender;		//index 3
@@ -28,14 +28,14 @@ public class Customer{
    private String addressZipCode;	//index 7
    private String phoneNumber;          //index 8
    private String emailAddress;		//index 9
-   private String password;		//index 10
+   private String password;		//index 10*/
    
    private final String notApplicable = "N/A";
    
-   public Customer(String newFirstName, String newLastName){
+   /*public Customer(String newFirstName, String newLastName){
       this.firstName = newFirstName;
       this.lastName = newLastName;
-   }
+   }*/
    
    public Customer(ArrayList<String> newCustomerInfoList) throws Exception{
       if(newCustomerInfoList.size() != 11){
@@ -45,62 +45,107 @@ public class Customer{
       this.customerInfoList =  newCustomerInfoList;
    }
    
+   private boolean validCustomerAttribute(int index){
+       boolean result = false;
+       
+       try{
+           if(index >= 0 && index <= 10 && this.customerInfoList != null){
+               if(this.customerInfoList.size() == 11
+                    && this.customerInfoList.get(index) != null){
+                   result = true;
+               }
+           }
+       }
+       catch(Exception x){
+            //display and log error
+       }
+       
+       return result;
+   }//end: validCustomerAttribute()
+   
+   private boolean setCustomerAttribute(int index, String newValue){
+       boolean result = false;
+       
+       try{
+           if(index >= 0 
+                   && index <= 10 
+                   && this.customerInfoList != null
+                   && newValue != null){
+               if(this.customerInfoList.size() == 11){
+                   this.customerInfoList.set(index, newValue);
+                   result = true;
+               }
+           }
+       }
+       catch(Exception x){
+           //display and log error
+       }
+       
+       return false;
+   }
+   
    public String getFirstName(){
-	   return (this.firstName == null ? notApplicable : this.firstName);
+       //return (this.firstName == null ? notApplicable : this.firstName);
+       return(validCustomerAttribute(0) ? notApplicable : this.customerInfoList.get(0));
    }
    
    public String getLastName(){
-	   return (this.lastName == null ? notApplicable : this.firstName);
+       //return (this.lastName == null ? notApplicable : this.firstName);
+       return(validCustomerAttribute(1) ? notApplicable : this.customerInfoList.get(1));
+         
    }
    
    public String getDateOfBirth(){
-	   return (this.dateOfBirth == null ? notApplicable : this.dateOfBirth);
+       //return (this.dateOfBirth == null ? notApplicable : this.dateOfBirth);
+       return(validCustomerAttribute(2) ? notApplicable : this.customerInfoList.get(2));
    }
    
    public String getGender(){
-	   return (this.gender == null ? notApplicable : this.gender);
+       //return (this.gender == null ? notApplicable : this.gender);
+       return(validCustomerAttribute(3) ? notApplicable : this.customerInfoList.get(3));
    }
    
    public String getPhysicalAddress(){
-	   return (this.physicalAddress == null ? notApplicable : this.physicalAddress);
+       //return (this.physicalAddress == null ? notApplicable : this.physicalAddress);
+       return(validCustomerAttribute(4) ? notApplicable : this.customerInfoList.get(4));
    }
    
    public String getAddressCity(){
-	   return (this.addressCity == null ? notApplicable : this.addressCity);
+       //return (this.addressCity == null ? notApplicable : this.addressCity);
+       return(validCustomerAttribute(5) ? notApplicable : this.customerInfoList.get(5));
    }
    
    public String getAddressState(){
-	   return (this.addressState == null ? notApplicable : this.addressState);
+       //return (this.addressState == null ? notApplicable : this.addressState);
+       return(validCustomerAttribute(6) ? notApplicable : this.customerInfoList.get(6));
    }
    
    public String getAddressZipCode(){
-	   return (this.addressZipCode == null ? notApplicable : this.addressZipCode);
+       //return (this.addressZipCode == null ? notApplicable : this.addressZipCode);
+       return(validCustomerAttribute(7) ? notApplicable : this.customerInfoList.get(7));
    }
    
    public String getPhoneNumber(){
-       return(this.phoneNumber == null ? notApplicable : this.phoneNumber);
+       //return(this.phoneNumber == null ? notApplicable : this.phoneNumber);
+       return(validCustomerAttribute(8) ? notApplicable : this.customerInfoList.get(8));
    }
    
    public String getEmailAddress(){
-	   return (this.emailAddress == null ? notApplicable : this.emailAddress);
+       //return (this.emailAddress == null ? notApplicable : this.emailAddress);
+       return(validCustomerAttribute(9) ? notApplicable : this.customerInfoList.get(9));
    }
    
    public void setFirstName(String newFirstName) throws Exception{
-	   if(newFirstName != null){
-		   this.firstName = newFirstName;
-	   }
-	   else{
-		   throw new Exception("Setting Customer Value Error; Incorrect First Name");
-	   }
+       if(newFirstName == null || !setCustomerAttribute(0, newFirstName)){
+           //this.firstName = newFirstName;
+           throw new Exception("Setting Customer Value Error; Incorrect First Name");
+       }
    }
    
    public void setLastName(String newLastName) throws Exception{
-	   if(newLastName != null){
-		   this.lastName = newLastName;
-	   }
-	   else{
-		   throw new Exception("Setting Customer Value Error; Incorrect Last Name");
-	   }
+       if(newLastName == null || !setCustomerAttribute(1, newLastName)){
+           throw new Exception("Setting Customer Value Error; Incorrect Last Name");
+       }
    }
    
    public void setDateOfBirth(String newDateOfBirth) throws Exception{
@@ -182,6 +227,14 @@ public class Customer{
 	   else{
 		   throw new Exception("Setting Customer Value Error; Incorrect Password");
 	   }
+   }
+   
+   private void saveCustomerInfo() throws Exception{
+       
+   }
+   
+   private static void loadCustomerInfo() throws Exception{
+       
    }
  
  }
