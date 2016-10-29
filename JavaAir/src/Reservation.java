321e96f440d32a2c35f515be841b934a4783a138
Steve Jia <ssjia@pnw.edu>
2016-10-29 12:08:58 -0500

    updated Reservation.java and created Passenger.java

--- JavaAir/src/Reservation.java
+++ JavaAir/src/Reservation.java
@@ -1,13 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Le
+/* 
+ * CS5900 - Software Engineering
+ * Professor: Dr. Ruijian Zhang
+ * Project: Java Air
+ * Team: Avian Limited
+ * 
+ * Filename: Reservation.java
+ * Author: Steve Jia 
+ * Creation: 2016-10-29
+ * 
+ * Changelog:
+ * 
  */
 public class Reservation {
+    private Flight originFlight;
+    private Flight returnFlight;
+    
+    //comment below section later
+    private String flighNumber;
+    private String origin;
+    private String destination;
+    private String departTime;
+    private String arrivalTime;
+    //comment above section later
+    
+    //private ArrayList<Passenger> passengerList;
+    private int numberOfCheckedBags;
+    private String paymentType;
+    private int pointsRedeemed;
+    private float amountPaid;
+    private String billingAddress;
+    private String billingPhone;
+    private String billingEmail;
+    
+    public Reservation(){
+        
+    }//end constructor
+    
     
 }
