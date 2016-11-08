ede8093c156cabfc7a5ce5963035d6a7a46f3287
zhan1286 <zhan1286@pnw.edu>
2016-11-08 17:20:44 -0600

    create priceCalculator,java

---
+++ JavaAir/src/java_air/panel/reservation/PriceCalculator.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_air.panel.reservation;
+
+import java.text.DecimalFormat;
+import java_air.main.Global;
+
+/**
+ *
+ * @author Rui Zhang
+ */
+public class PriceCalculator {
+    private double fare;
+    private double taxFee;
+    private double totalFee;
+    private int rewardPointUse = 0;
+    private int rewardRate2Cash = 1000;
+    private DecimalFormat decimalFormat = new DecimalFormat(".##");
+    public PriceCalculator(){
+        fare = (Global.currentReservation.getOriginFlight().getTripMileage() * 0.27
+               * Global.currentReservation.getNumberOfPassenger());
+        taxFee =  (fare*0.1432);
+        totalFee = fare + taxFee;
+    }
+    public String getFare(){
+        return ("$" + decimalFormat.format(fare));
+    }
+    public String getTaxFee(){
+        return ("$" + decimalFormat.format(taxFee));
+    }
+    public String getTotalFee(){
+        return ("$" + decimalFormat.format(totalFee));
+    }
+    public String getRewardRedeem(){
+        return ("$" + this.rewardPointUse/rewardRate2Cash);
+    }
+    public void setRewardPointUse(int rewardPointUse){
+        this.rewardPointUse = rewardPointUse;
+        totalFee -= this.rewardPointUse/rewardRate2Cash;
+    }
+
+}
