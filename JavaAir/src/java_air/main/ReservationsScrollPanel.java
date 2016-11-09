3eec0ea8f8ad64f54656a9459aeac538cac0caac
Steve Jia <ssjia@pnw.edu>
2016-11-08 20:56:17 -0600

    added DB query and partial data selection code for ReservationsScrollPanel.java

--- JavaAir/src/java_air/main/ReservationsScrollPanel.java
+++ JavaAir/src/java_air/main/ReservationsScrollPanel.java
@@ -15,6 +15,9 @@ package java_air.main;
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java_air.database.DataClient;
+import java_air.panel.reservation.Reservation;
 import javax.swing.*;
 
 public class ReservationsScrollPanel extends JPanel{
@@ -27,9 +30,53 @@ public class ReservationsScrollPanel extends JPanel{
         BoxLayout layout = new BoxLayout(this,BoxLayout.Y_AXIS); 
         this.setLayout(layout);
         
-        for(int i=1;i<5;i++){
-        ReservationInfoPanel aReservationInfoPanel = new ReservationInfoPanel();
-        add(aReservationInfoPanel);
+        //check to make sure logged in
+        if(Global.currentCustomer == null){
+            JOptionPane.showMessageDialog(null
+                    , "No Currently Logged In Customer"
+                    , "Must Be Logged In"
+                    , JOptionPane.WARNING_MESSAGE);
+            Global.switchCard(Global.textLogin);
         }
+        
+        //get a list of reservations from db
+        String query = "SELECT R.TICKETID AS TicketID" +
+                ", R.RESERVATIONNUMBER AS ReservationNumber" +
+                ", R.FLIGHTNUMBER AS FlightNumber" +
+                ", F.ORIGINAIRPORT AS OriginAirport" +
+                ", F.DESTINATIONAIRPORT AS DestinationAirport" +
+                ", R.DEPARTDATETIME AS DepartDate" +
+                ", R.ARRIVALDATETIME AS ArrivalDate" +
+                ", F.SCHEDULEDDEPARTURETIME AS DepartTime" +
+                ", F.SCHEDULEDARRIVETIME AS ArrivalTime" +
+                ", F.AIRCRAFTTYPE AS AircraftType" +
+                ", F.FLIGHTDURATION AS FlightDuration" +
+                ", R.CUSTOMERID AS CustomerID" +
+                ", R.COST AS Cost" +
+                ", R.CASHPAID AS CashPaid" +
+                ", R.REWARDSUSED AS RewardsUsed " +
+                "FROM RESERVATIONS R INNER JOIN FLIGHTS F " +
+                "ON R.FLIGHTNUMBER = F.FLIGHTNUMBER " +
+                "WHERE R.CUSTOMERID = " + Global.currentCustomer.getCustomerID();
+        
+        ArrayList<Reservation> reservList = null;
+        try{
+            reservList = DataClient.getReservationData(query);
+        }
+        catch(Exception x){
+            return;
+        }//end try-catch
+        
+        if(reservList != null && !reservList.isEmpty()){
+            for(Reservation reserv : reservList){
+                //ReservationInfoPanel infoPanel = new ReservationInfoPanel(reserv);
+                //add(infoPanel);
+            }//end for loop
+        }//end if
+        
+        /*for(int i=1;i<5;i++){
+            ReservationInfoPanel aReservationInfoPanel = new ReservationInfoPanel();
+            add(aReservationInfoPanel);
+        }*/
     }
 }
