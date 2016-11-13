4af0da8c2d2be78d9ec02f7408d131463b69dc42
Xu Wu <wu250@pnw.edu>
2016-11-13 16:49:47 -0600

    Added a new test for Customer.java

--- JavaAir/test/java_air/main/CustomerTest.java
+++ JavaAir/test/java_air/main/CustomerTest.java
@@ -2,19 +2,43 @@ package java_air.main;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class CustomerTest {
 
 	@Test
-	public void setDateOfBirth_testFormat() {
-		Customer customer = new Customer(); 
-				try{
-					panel.setEmailLabelText("ABcd332");
-					fail();
-					}catch (Exception e){
-						assertTrue(true);
-					}
+	// Based on SRS v4.2 Section 3.2.2.1
+	public void setDateOfBirth_testFormatNormal() throws Exception {
+		ArrayList<String> customerInfo = new ArrayList<String>();
+		/*
+		 * private String firstName; //index 0 private String lastName; //index
+		 * 1 private String dateOfBirth; //index 2 private String gender;
+		 * //index 3 private String physicalAddress; //index 4 private String
+		 * addressCity; //index 5 private String addressState; //index 6 private
+		 * String addressZipCode; //index 7 private String phoneNumber; //index
+		 * 8 private String emailAddress; //index 9 private String password;
+		 * //index 10
+		 */
+		customerInfo.add("John");
+		customerInfo.add("Doe");
+		customerInfo.add("1988-01-01");
+		customerInfo.add("male");
+		customerInfo.add("2200 169th St");
+		customerInfo.add("Hammond");
+		customerInfo.add("IN");
+		customerInfo.add("46323");
+		customerInfo.add("2191234567");
+		customerInfo.add("johndoe@gmail.com");
+		customerInfo.add("123");
+		Customer customer = new Customer(customerInfo);
+		try {
+			customer.setDateOfBirth("1988-01-01");
+			assertTrue(true);
+		} catch (Exception e) {
+			fail();
+		}
 	}
 
 }
