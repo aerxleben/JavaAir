3452acd3b392fc4bb5b7d0573d0ee8b2cd0cf5a8
moscatel-1 <moscatel.1@osu.edu>
2016-11-20 15:37:31 -0600

    Add files via upload

---
+++ JavaAir/test/java_air/main/ReservationInfoPanelTest.java
@@ -0,0 +1,246 @@
+
+package java_air.main;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matt
+ */
+public class ReservationInfoPanelTest {
+    
+    public ReservationInfoPanelTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getReservationNumberLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReservationNumberLabel() {
+        System.out.println("getReservationNumberLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReservationNumberLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getDepartFlightNumberLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetDepartFlightNumberLabel() {
+        System.out.println("getDepartFlightNumberLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getDepartFlightNumberLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnFlightNumberLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnFlightNumberLabel() {
+        System.out.println("getReturnFlightNumberLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnFlightNumberLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getOriginLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetOriginLabel() {
+        System.out.println("getOriginLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getOriginLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getDestinationLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetDestinationLabel() {
+        System.out.println("getDestinationLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getDestinationLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnOriginLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnOriginLabel() {
+        System.out.println("getReturnOriginLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnOriginLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnDestinationLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnDestinationLabel() {
+        System.out.println("getReturnDestinationLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnDestinationLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getDepartureLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetDepartureLabel() {
+        System.out.println("getDepartureLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getDepartureLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getArrivalLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetArrivalLabel() {
+        System.out.println("getArrivalLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getArrivalLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnDepartureLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnDepartureLabel() {
+        System.out.println("getReturnDepartureLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnDepartureLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnArrivalLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnArrivalLabel() {
+        System.out.println("getReturnArrivalLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnArrivalLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getDurrationLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetDurrationLabel() {
+        System.out.println("getDurrationLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getDurrationLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnDurationLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnDurationLabel() {
+        System.out.println("getReturnDurationLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getReturnDurationLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getPassengerLabel method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetPassengerLabel() {
+        System.out.println("getPassengerLabel");
+        ReservationInfoPanel instance = null;
+        JLabel expResult = null;
+        JLabel result = instance.getPassengerLabel();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getCheckInButton method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetCheckInButton() {
+        System.out.println("getCheckInButton");
+        ReservationInfoPanel instance = null;
+        JButton expResult = null;
+        JButton result = instance.getCheckInButton();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+
+    /**
+     * Test of getReturnCheckInButton method, of class ReservationInfoPanel.
+     */
+    @Test
+    public void testGetReturnCheckInButton() {
+        System.out.println("getReturnCheckInButton");
+        ReservationInfoPanel instance = null;
+        JButton expResult = null;
+        JButton result = instance.getReturnCheckInButton();
+        assertEquals(expResult, result);
+        fail("No DB connection made.");
+    }
+    
+}
