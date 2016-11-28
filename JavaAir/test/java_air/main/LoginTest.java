1f8fbebe0424336008bca2b2f8d8749814b96507
guoyu qi <gqi@pnw.edu>
2016-11-29 06:34:03 +0800

    registered customer new reservation

--- JavaAir/test/java_air/main/LoginTest.java
+++ JavaAir/test/java_air/main/LoginTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 /**
  *
  * @author Yuwei Cao
+ * @author Guoyu Qi
  */
 public class LoginTest {
     @Test
@@ -21,6 +22,23 @@ public class LoginTest {
         LoginLandingPanel panel = new LoginLandingPanel();
         panel.getEmailField().setText("TestRegistration@pnw.edu");
         panel.getPasswordField().setText("11111111");
+        try{
+            panel.loginButtonPressed();
+        }
+        catch(Exception e){
+        }
+        
+        assertTrue(("TestRegistrationFN".equals(Global.currentCustomer.getFirstName())) && ("TestRegistrationLN".equals(Global.currentCustomer.getLastName())));    
+   
+    }
+    
+        public void login_TestNormal1() throws Exception{
+        //register new test customer
+        new RegistrationTest().registration_TestNormal();
+        //test customer login
+        LoginLandingPanel panel = new LoginLandingPanel();
+        panel.getEmailField().setText("logintest@gmail.com");
+        panel.getPasswordField().setText("123456");
         try{
             panel.loginButtonPressed();
         }
--- JavaAir/test/java_air/main/RegistrationTest.java
+++ JavaAir/test/java_air/main/RegistrationTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 /**
  *
  * @author Yuwei Cao
+ * @author Guoyu Qi
  */
 public class RegistrationTest {
     
@@ -59,6 +60,64 @@ public class RegistrationTest {
         panel.getEmailField().setText("TestRegistration@pnw.edu");
         panel.getPasswordField().setText("11111111");
         panel.getCPasswordField().setText("11111111");
+        
+        try{
+            panel.getSubmitButton().doClick();
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null
+                    , ex.getMessage()
+                    , "Click submit button error."
+                    , JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+       
+        assertTrue(new DataClient().getCount(existCustomerQuery) > 0);
+        
+    }
+    
+        @Test
+    public void registration_TestNormal1() throws Exception{
+        
+        //delete existing test customer and test reservation
+        String existCustomerQuery = "SELECT CustomerID FROM Customers WHERE FIRSTNAME = 'TestRegistrationFN' AND LASTNAME = 'TestRegistrationLN' LIMIT 1";
+        int existId = new DataClient().getCount(existCustomerQuery);
+        if(existId > 0){
+            JOptionPane.showMessageDialog(null, "There are existing test customer.");
+            String deleteCustomerQuery = "DELETE FROM Customers WHERE CustomerID = '" + existId + "'";
+            String existReservationQuery =  "SELECT ReservationNumber FROM Reservations WHERE CustomerID = '"+ existId + "'";
+            String deleteReservationQuery = "DELETE FROM Reservations WHERE CustomerID = '"+ existId + "'";
+            new DataClient().dbInsertOrUpdate(deleteCustomerQuery);
+            JOptionPane.showMessageDialog(null, "Existing test customer deleted.");
+       
+            try{
+                new DataClient().dbInsertOrUpdate(deleteReservationQuery);
+                JOptionPane.showMessageDialog(null, "Existing test reservation deleted.");
+            }catch(Exception ex){
+               // JOptionPane.showMessageDialog(null
+               //         , ex.getMessage()
+               //         , "DB error."
+               //        , JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "No test customer existing.");
+        }
+        
+        //register new test customer
+        RegistrationPanel panel = new RegistrationPanel();
+        panel.getFirstField().setText("TestFristName");
+        panel.getLastField().setText("TestLastName");
+        panel.getBirthdayPicker().setDate(Global.dateFormat.parse("1996-12-12"));
+        panel.getGenderBox().setSelectedIndex(1);
+        panel.getAddressField().setText("2330 173RD ST Griffin000");
+        panel.getCityField().setText("Hammond");
+        panel.getStateField().setText("IN");
+        panel.getZipField().setText("463233");
+        panel.getPhoneField().setText("(321) 000-0000");
+        panel.getEmailField().setText("logintest@gmail.com");
+        panel.getPasswordField().setText("123456");
+        panel.getCPasswordField().setText("123456");
         
         try{
             panel.getSubmitButton().doClick();
